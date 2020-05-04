411d34ff061a6a96f0c0aad3e9769445a13aedfe
Bernardo Maluleque <b.a.maluleque@gmail.com>
2020-05-05 00:10:08 +0200

    feat: change details layout and support rotation

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020.  The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.udacity.sandwichclub.utils;
 
 import android.util.Log;
@@ -12,11 +29,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class JsonUtils {
 
     private static final String TAG = "JsonUtils";
 
+    /*
+     * Parse JSON to Sandwich object
+     * */
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject sandwichJsonObject = new JSONObject(json);
@@ -24,11 +43,11 @@ public class JsonUtils {
 
             String mainName = name.getString("mainName");
 
-            List<String> alsoKnowns = new ArrayList<>();
-            JSONArray alsoKnownsArray = name.getJSONArray("alsoKnownAs");
-            if (alsoKnownsArray != null) {
-                for (int i = 0; i < alsoKnownsArray.length(); i++) {
-                    alsoKnowns.add(alsoKnownsArray.getString(i));
+            List<String> alsoKnownAs = new ArrayList<>();
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            if (alsoKnownAsArray != null) {
+                for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                    alsoKnownAs.add(alsoKnownAsArray.getString(i));
                 }
             }
 
@@ -36,15 +55,15 @@ public class JsonUtils {
             String description = sandwichJsonObject.getString("description");
             String image = sandwichJsonObject.getString("image");
 
-            List<String> igredients = new ArrayList<>();
-            JSONArray igredientsArray = sandwichJsonObject.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            JSONArray ingredientsArray = sandwichJsonObject.getJSONArray("ingredients");
 
-            if (igredientsArray != null) {
-                for (int i = 0; i < igredientsArray.length(); i++) {
-                    igredients.add(igredientsArray.getString(i));
+            if (ingredientsArray != null) {
+                for (int i = 0; i < ingredientsArray.length(); i++) {
+                    ingredients.add(ingredientsArray.getString(i));
                 }
             }
-            return new Sandwich(mainName, alsoKnowns, placeOfOrigin, description, image, igredients);
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
 
         } catch (JSONException e) {
             Log.e(TAG, "parseSandwichJson: " + e.getMessage());
